1e368917803166f435a6fe3969f1c3dc1a8add3f
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-04-06 17:36:32 -0600

    Add css and change ui

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const bookshelf = document.querySelector('.libraryBoxes');
 const newBookButton = document.querySelector('.newBookButton');
 const dialog = document.querySelector('dialog');
 
-const exitButton = document.querySelector('.exitButton');
 const submitButton = document.querySelector('.submitButton');
 
 const titleInput = document.querySelector('#title');
@@ -15,19 +14,14 @@ let bookNumber = 0;
 const myLibrary = [];
 
 function Book(title, author, pages, read, index) {
-  // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.index = index;
-    this.info = () => {
-    return `{this.title}, {this.author}, {this.pages}, {this.read}`;
-  }
 }
 
 function addBookToLibrary(bookData) {
-  // do stuff here
     myLibrary.push(bookData);
     newBook = bookshelf.appendChild(document.createElement('div'));
     newBook.classList.add('bookContainer');
@@ -37,20 +31,18 @@ function addBookToLibrary(bookData) {
     authorText.textContent = bookData.author;
     pagesText = newBook.appendChild(document.createElement('p'));
     pagesText.textContent = bookData.pages;
-    readStatusText = newBook.appendChild(document.createElement('p'));
-    readStatusText.textContent = bookData.read;
+    readStatusButton = newBook.appendChild(document.createElement('button'));
+    readStatusButton.textContent = bookData.read ? "read" : "not read yet";
+    readStatusButton.className = bookData.read ? "green" : "red";
+    readStatusButton.addEventListener('click', () => {
+        changeRead(bookData.index);
+    });
     removeButton = newBook.appendChild(document.createElement('button'));
     removeButton.textContent = 'Remove book';
     removeButton.addEventListener('click', () => {
         removeBook(bookData.index);
     });
     bookNumber++;
-}
-
-function displayBook() {
-    for (let i=0; i<myLibrary.length; i++) {
-        console.log(myLibrary[i]);
-    }
 }
 
 function removeBook(bookIndex) {
@@ -62,33 +54,32 @@ function removeBook(bookIndex) {
     }
 }
 
+function changeRead(bookIndex) {
+    let status = myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+    readStatusButton = bookshelf.childNodes[bookIndex].childNodes[3];
+    readStatusButton.textContent = status ? "read" : "not read yet";
+    readStatusButton.className = status ? "green" : "red";
+}
+
 newBookButton.addEventListener('click', () => {
     dialog.showModal();
 });
 
-exitButton.addEventListener('click', () => {
-    dialog.close();
-});
+dialog.addEventListener('click', function(event) {
+    let rect = dialog.getBoundingClientRect();
+    let isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+      rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+    if (!isInDialog) {
+      dialog.close();
+    }
+  });
 
 submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    let readStatus;
-    if (readStatusInput.checked) {
-        readStatus = 'read';
-    }
-    else {
-        readStatus = 'not read yet';
-    }
     dialog.close();
-    addBookToLibrary(new Book(titleInput.value, authorInput.value, pagesInput.value, readStatus, bookNumber));
+    addBookToLibrary(new Book(titleInput.value, authorInput.value, pagesInput.value, readStatusInput.checked, bookNumber));
     titleInput.value = null;
     authorInput.value = null;
     pagesInput.value = null;
     readStatusInput.checked = false;
 });
-
-// addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet'));
-// addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet'));
-
-
-// displayBook();
